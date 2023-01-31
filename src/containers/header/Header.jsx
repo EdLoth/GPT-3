ff0a8173b7037358144fd5a96640f40a56d40d0d
Eduardo Ramos <eduardosram10@gmail.com>
2023-01-31 12:19:32 -0300

    Resolution Warnings

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -15,13 +15,12 @@ const Header = () => (
       </div>
 
       <div className="gpt3__header-content__people">
-        <img src={people} />
+        <img src={people} alt='people bg'/>
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
-
     <div className="gpt3__header-image">
-      <img src={ai} />
+      <img src={ai} alt='ia bg'/>
     </div>
   </div>
 );
